ad0e968e71cc1e5e8cf77a11518b339d81da8d14
Sachet Adhikari <66741255+SachetAdhikari@users.noreply.github.com>
2021-08-18 23:13:53 +0545

    dicee

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,76 @@
 import 'package:flutter/material.dart';
-import 'input_page.dart';
+import 'dart:math';
 
-void main() => runApp(BMICalculator());
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
 
-class BMICalculator extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1 + (Random().nextInt(6));
+  int rightDiceNumber = 1 + (Random().nextInt(6));
+
+  void generateDice() {
+    setState(() {
+      leftDiceNumber = 1 + (Random().nextInt(6));
+      rightDiceNumber = 1 + (Random().nextInt(6));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // theme: ThemeData(
-      //   primaryColor: Color(0xFF0A0E21),
-      //   scaffoldBackgroundColor: Color(0xFF0A0E21),
-      //   accentColor: Colors.purple,
-      //   textTheme: TextTheme(
-      //     body1: TextStyle(color: Colors.white),
-      //   ),
-      // ),
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+    // var leftDiceNumber = 1;
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            // flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  generateDice();
+                  print(
+                      'Left button pressed : $leftDiceNumber,$rightDiceNumber');
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            // flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  generateDice();
+                  print(
+                      'Right button pressed: $rightDiceNumber,$leftDiceNumber');
+                },
+                child: Image(
+                  image: AssetImage('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      home: InputPage(),
     );
   }
 }
